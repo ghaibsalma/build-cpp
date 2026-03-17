14ccf7262fd7dd55eb3d9f9d919cdc52d1bc91ac
Salma El Ghaib <sel-ghai@c2r6p13.1337.ma>
2026-03-17 18:26:20 +0000

    fix: use NULL-terminated loop for to_build->classes iteration

--- builder.c
+++ builder.c
@@ -3,7 +3,7 @@
 void	ft_make_files(t_build *to_build, int size) {
 	int	i = 0;
 
-	while (i < size) {
+	while (to_build->classes[i]) {
 		ft_make_header(to_build, i);
 		ft_make_source(to_build, i);
 		i++;
