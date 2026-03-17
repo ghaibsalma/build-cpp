a0c1e2be4c02a8cd5e9dbdb459cfddb9c29ef9a5
Salma El Ghaib <sel-ghai@c2r6p13.1337.ma>
2026-03-17 18:28:12 +0000

    feat: add parser function to fill flags array

--- parser.c
+++ parser.c
@@ -22,11 +22,12 @@ int		is_not_name(char *s) {
 	return (0);
 }
 
-int		fill(int i, t_class **c, char** av) {
-	c[i] = malloc(sizeof(t_class));
-	c[i]->name = strdup(av[i]);
-	if (!c[i]->name || is_not_name(c[i]->name))
+int		fill(int *i, t_class **c, char* av) {
+	c[*i] = malloc(sizeof(t_class));
+	c[*i]->name = strdup(av);
+	if (!c[*i]->name || is_not_name(c[*i]->name))
 		return (0);
+	*i += 1;
 	return (1);
 }
 
@@ -43,15 +44,28 @@ int		free_classes(t_class **c, int id) {
 	return (1);
 }
 
+int		flag(char *f, char *arg) {
+	if (arg[0] != '-' || strlen(arg) != 2 || !*strchr("hom", arg[1]))
+		return (0);
+	if (arg[1] == 'h')
+		f[0] = HELP_FLAG;
+	else if (arg[1] == 'o' && !f[1])
+		f[1] = ORTHDX_FLAG;
+	else if (arg[1] == 'm' && !f[1])
+		f[1] = MIN_FLAG;
+	return (1);
+}
+
 int		parser(int ac, char **av, t_build *to_build) {
 	int		i = 0;
+	int		j = 0;
 
 	to_build->classes = malloc(sizeof(t_class*) * (ac + 1));
 	while (i < ac) {
-		if (!fill(i, to_build->classes, av))
-			return (free_classes(to_build->classes, i));
+		if (!flag(to_build->flags, av[i]) && !fill(&j, to_build->classes, av[i]))
+			return (free_classes(to_build->classes, j));
 		i++;
 	}
-	to_build->classes[i] = NULL;
+	to_build->classes[j] = NULL;
 	return (0);
 }
